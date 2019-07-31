11237246177b249b878eba9e5a22427088021733
Luke Olson <lukeolson@Lukes-MacBook-Pro.local>
2019-07-30 17:17:40 -0700

    some jest testing

--- app/javascript/components/__tests__/Category.test.js
+++ app/javascript/components/__tests__/Category.test.js
@@ -7,13 +7,33 @@ import Category from "../Category";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Category>", () => {
+  const addRating = () => {
+    console.log("hello there");
+  };
+  const current_user = {
+    id: 1
+  };
+  const user = {
+    id: 1
+  };
   const category = {
     category_name: "Rails",
     id: 3
   };
-  const wrapper = shallow(<Category category={category} />);
+  const wrapper = shallow(
+    <Category
+      addRating={addRating}
+      category={category}
+      current_user={current_user}
+      user={user}
+    />
+  );
 
   it("renders without crashing", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  it("shows current rating when number is clicked", () => {
+    wrapper.setState({ rating: { score: 0 } });
+    expect(wrapper.find("h2").text()).toEqual("0");
   });
 });
