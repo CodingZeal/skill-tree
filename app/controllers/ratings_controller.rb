9ed484f328d53b2342c390c7ee890aea324a1aaf
Luke Olson <lukeolson@Lukes-MacBook-Pro.local>
2019-07-31 16:58:34 -0700

    more rubocop on ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,58 +2,59 @@
 
 # deals with ratings
 class RatingsController < ApplicationController
-
   def index
-    @ratings = []
+    ratings = []
     Rating.find_each do |rating|
-      @ratings << rating.as_json(include: { category: {} })
+      ratings << rating.as_json(include: { category: {} })
     end
-    render json: @ratings
+    render json: ratings
   end
 
   def my_ratings
-    @ratings = Rating.where(developer_id: params[:user_id]).map do |rating|
+    ratings = Rating.where(developer_id: params[:user_id]).map do |rating|
       rating.as_json(include: { category: {} })
     end
-    render json: @ratings
+    render json: ratings
   end
 
   def my_last_rating
-    @my_last_ratings = []
+    last_ratings_in_each_category = []
     Category.find_each do |category|
-      @all_my_ratings = Rating.where('developer_id = ? and mentor_id = ? and category_id = ?',
-                                      params[:user_id],
-                                      params[:user_id],
-                                      category.id).as_json(include: { category: {} })
-      if @all_my_ratings.length > 0
-        @my_last_ratings << @all_my_ratings.last
-      end
+      all_my_ratings = Rating.where(
+        'developer_id = ? and mentor_id = ? and category_id = ?',
+        params[:user_id],
+        params[:user_id],
+        category.id
+      )
+                             .as_json(include: { category: {} })
+      all_my_ratings.length.positive? && last_ratings_in_each_category << all_my_ratings.last
     end
-    render json: @my_last_ratings
+    render json: my_last_ratings
   end
 
   def my_static_ratings
-    @my_last_ratings = []
-    @user = User.where('unique_url = ?', params[:unique_url]).first
+    last_ratings_in_each_category = []
+    user = User.where('unique_url = ?', params[:unique_url]).first
     Category.find_each do |category|
-      @all_my_ratings = Rating.where('developer_id = ? and mentor_id = ? and category_id = ?',
-                                      @user.id,
-                                      @user.id,
-                                      category.id).as_json(include: { category: {} })
-      if @all_my_ratings.length > 0
-        @my_last_ratings << @all_my_ratings.last
-      end
+      all_my_ratings = Rating.where(
+        'developer_id = ? and mentor_id = ? and category_id = ?',
+        user.id,
+        user.id,
+        category.id
+      )
+                             .as_json(include: { category: {} })
+      all_my_ratings.length.positive? && last_ratings_in_each_category << all_my_ratings.last
     end
-    render json: @my_last_ratings
+    render json: my_last_ratings
   end
 
   def create
-    @rating = Rating.create(rating_params)
-    if @rating.valid?
-      render json: @rating
+    rating = Rating.create(rating_params)
+    if rating.valid?
+      render json: rating
     else
-      render json: @rating.errors, status: :unprocessable_entity
-      p @rating.errors
+      render json: rating.errors, status: :unprocessable_entity
+      p rating.errors
     end
   end
 
