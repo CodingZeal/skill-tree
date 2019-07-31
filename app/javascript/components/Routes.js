046246ee85adb49e05f316f520ffab04fdabcb02
Luke Olson <lukeolson@Lukes-MacBook-Pro.local>
2019-07-31 13:55:16 -0700

    added snapshots and fixed routes

--- app/javascript/components/Routes.js
+++ app/javascript/components/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Profile from "./Profile";
+import RankMyself from "./RankMyself";
 import StaticProfile from "./StaticProfile";
 
 class Routes extends React.Component {
